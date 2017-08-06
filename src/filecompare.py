ae38980eac968a930527ec3047b28d57e25728ee
jxauyhj@gmail.com <Yhj001401>
2017-08-06 21:49:16 +0800

    first version of the test file
    filecompare function

--- src/filecompare.py
+++ src/filecompare.py
@@ -1,5 +1,8 @@
 import filecmp
+import sys
+
 filecmp.cmp("/projects/python/vepython/lib/python2.7/os.py", "/projects/python/vepython/lib/python2.7/re.py")
 print 'next'
-filecmp.dircmp("/projects/python/vepython/lib","/projects/python/vepython/lib")
+filecmp.dircmp("/projects/python/vepython/lib", "/projects/python/vepython/lib")
 
+sys.exit()
